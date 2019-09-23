9de1156cb67539f08bdf6825b83604fa298b4ce2
valera21115 <55016857+valera21115@users.noreply.github.com>
2019-09-23 14:45:52 +0300

    Add files via upload

--- Lab2_15/src/Main.java
+++ Lab2_15/src/Main.java
@@ -7,11 +7,25 @@ public class Main
     {
      int row;
      int col;
+
      Scanner in=new Scanner(System.in);
+
+     try
+     {
      System.out.println("Enter count of rows: ");
      row=in.nextInt();
+     if(row<=0)
+     {
+         System.out.println("Error! Count of rows must be positive");
+         return;
+     }
      System.out.println("Enter count of columns:");
      col=in.nextInt();
+     if(col<=0)
+     {
+             System.out.println("Error! Count of columns must be positive");
+             return;
+     }
      int [][] arr=new int[row][col];
 
         FillArray.fillArr(arr);
@@ -20,5 +34,10 @@ public class Main
         SortCol.sortCol(0,row-1,arr);
         System.out.println("Sorted by first elements of matrix rows: ");
         ShowArr.showArr(arr);
+     }
+     catch (Throwable wrongType){
+        System.out.println("Error occurred while reading input!");
+        return;
+    }
     }
 }
